2923754dddbff885d9f61fffdd922e19398c4340
cdlara <cdlara@sunspiresolutions.com>
2019-10-17 23:56:36 +0800

    edit_wkhtml

--- config/snappy.php
+++ config/snappy.php
@@ -5,13 +5,13 @@ return array(
 
     'pdf' => array(
         'enabled' => true,
-        'binary' => base_path('vendor\wemersonjanuario\wkhtmltopdf-windows\bin\64bit\wkhtmltopdf'),
+        'binary' => base_path('wkhtmltopdf\bin\wkhtmltopdf'),
         // 'binary' => 'C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf',
         'options' => array(),
     ),
     'image' => array(
         'enabled' => true,
-        'binary' => base_path('vendor\wemersonjanuario\wkhtmltopdf-windows\bin\64bit\wkhtmltoimage'),
+        'binary' => base_path('wkhtmltopdf\bin\wkhtmltoimage'),
         // 'binary' => 'C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltoimage',
         'options' => array(),
     ),
